af82d7be5667603373613c55f8e3ea009b2bc401
emlynw <emlynw98@gmail.com>
2024-01-16 16:26:05 +0000

    joint position control

--- gym-env-test.py
+++ gym-env-test.py
@@ -1,13 +1,13 @@
 import gymnasium as gym
 from gymnasium.wrappers import PixelObservationWrapper
-from wrappers import FrameStack
 import cv2
 
 import gym_INB0104
+import numpy as np
 
 def main():
-    env = gym.make("gym_INB0104/INB0104-v0", render_mode="human")
-    # env = PixelObservationWrapper(env, pixels_only=False)
+    env = gym.make("gym_INB0104/INB0104-v0", render_mode="rgb_array")
+    env = PixelObservationWrapper(env, pixels_only=False)
     # env = FrameStack(env, num_frames=3)
     
     # # print the observation space
@@ -17,12 +17,22 @@ def main():
 
     # reset the environment
     obs, info = env.reset()
+    cv2.imshow("obs", cv2.cvtColor(obs['pixels'], cv2.COLOR_RGB2BGR))
+    cv2.waitKey(100)
     
+    high = True
     # render the environment
     while True:
-        for i in range(100):
-            obs, reward, terminated, truncated, info = env.step(env.action_space.sample())
-
+        for i in range(50):
+            if i % 100 == 0:
+                high = not high
+            if high:
+                action = env.action_space.high
+            else:
+                action = env.action_space.low
+            obs, reward, terminated, truncated, info = env.step(action)
+            cv2.imshow("obs", cv2.cvtColor(obs['pixels'], cv2.COLOR_RGB2BGR))
+            cv2.waitKey(10)
 
         env.reset()
 
